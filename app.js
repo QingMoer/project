f58aae781aa966e4e0265bab2c333a6a8a206c4d
capacitor <capacitorgh@riseup.net>
2023-12-07 20:33:08 -0500

    fix port quickly

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use('/new', createRouter);
 app.use('/delete', deleteRouter);
 
 
-const PORT = process.env.PORT || 6060;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running on  ${PORT}`));
 
